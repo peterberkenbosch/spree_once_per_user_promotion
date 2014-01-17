d942c91656ef4303f7fda68de47507eeec7fae34
Peter Berkenbosch <peter@pero-ict.nl>
2014-01-17 16:53:43 +0100

    we do not need to count the eligiblesÂ!

--- app/models/once_per_user.rb
+++ app/models/once_per_user.rb
@@ -3,10 +3,8 @@ require 'pry'
 class OncePerUser < Spree::Promotion::Rules::User
   def eligible?(order, options = {})
     user = order.user
-    # originator is the PromotionAction
     action_ids = promotion.actions.select(:id).collect(&:id)
-    promotion_count = Spree::Adjustment.promotion.eligible.where(adjustable_id: Spree::Order.select(:id).where(user_id: user.id).collect(&:id)).where(source_id: action_ids).count
-    #binding.pry
+    promotion_count = Spree::Adjustment.promotion.where(adjustable_id: Spree::Order.select(:id).where(user_id: user.id).collect(&:id)).where(source_id: action_ids).count
     users.include?(user) && promotion_count == 0
   end
 end
