2e4848a011bc64cb02c9535d3c7863bdbeec7441
Peter Berkenbosch <peter@pero-ict.nl>
2014-01-17 14:40:32 +0100

    debugging with pry...

--- app/models/once_per_user.rb
+++ app/models/once_per_user.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 class OncePerUser < Spree::Promotion::Rules::User
   def eligible?(order, options = {})
     user = order.user
     # originator is the PromotionAction
     action_ids = promotion.actions.select(:id).collect(&:id)
     promotion_count = Spree::Adjustment.promotion.eligible.where(adjustable_id: Spree::Order.select(:id).where(user_id: user.id).collect(&:id)).where(source_id: action_ids).count
+    #binding.pry
     users.include?(user) && promotion_count == 0
   end
 end
--- spree_once_per_user_promotion.gemspec
+++ spree_once_per_user_promotion.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
   s.requirements << 'none'
 
   s.add_dependency 'spree_core', '~> 2.2.0.beta'
+  s.add_dependency 'pry'
 
   s.add_development_dependency 'capybara', '~> 2.1'
   s.add_development_dependency 'coffee-rails'
