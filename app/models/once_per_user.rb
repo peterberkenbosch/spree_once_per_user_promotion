d25a0af66715c62f7878e9ddfff8e24f9940b1ee
Peter Berkenbosch <peter@pero-ict.nl>
2014-02-14 18:27:08 +0100

    no binding.pry left behind!

--- app/models/once_per_user.rb
+++ app/models/once_per_user.rb
@@ -4,9 +4,6 @@ class OncePerUser < Spree::Promotion::Rules::User
     user = order.user
     action_ids = promotion.actions.select(:id).collect(&:id)
     promotion_count = Spree::Adjustment.promotion.where(adjustable_id: Spree::Order.select(:id).where(user_id: user.id).collect(&:id)).where(source_id: action_ids).count
-
-    binding.pry
-
     users.include?(user) && promotion_count == 0 && promotion.code.downcase == order.coupon_code.downcase
   end
 end
