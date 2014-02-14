396b8ae155642dca4c96cb025f4cdbcffab924d1
Peter Berkenbosch <peter@pero-ict.nl>
2014-02-14 18:21:32 +0100

    check for coupon code here

--- app/models/once_per_user.rb
+++ app/models/once_per_user.rb
@@ -1,10 +1,12 @@
-require 'pry'
-
+require "pry"
 class OncePerUser < Spree::Promotion::Rules::User
   def eligible?(order, options = {})
     user = order.user
     action_ids = promotion.actions.select(:id).collect(&:id)
     promotion_count = Spree::Adjustment.promotion.where(adjustable_id: Spree::Order.select(:id).where(user_id: user.id).collect(&:id)).where(source_id: action_ids).count
-    users.include?(user) && promotion_count == 0
+
+    binding.pry
+
+    users.include?(user) && promotion_count == 0 && promotion.code.downcase == order.coupon_code.downcase
   end
 end
